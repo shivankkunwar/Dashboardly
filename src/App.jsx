de0801414ad739d67c5472d4503e56ca6c4d629a
shivank <shivankkunwar100@gmail.com>
2023-11-17 12:32:12 +0530

    completed Add account component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,106 @@
-import React, { useState } from 'react';
-import UserDetails from './Components/UserDetails/UserDetails';
-import AccountCreation from './Components/AccountCreation/AccountCreation';
+import React, { useState } from "react";
+import UserDetails from "./Components/UserDetails/UserDetails";
+import AccountCreation from "./Components/AccountCreation/AccountCreation";
 function App() {
-  const [tab, setTab] = useState('userDetails');
+  const [tab, setTab] = useState("userDetails");
+  let modal = document.getElementById("my-modal");
 
-  const Button = ({ label, value }) => (
-    <button 
-      style={{
-        padding: '10px 20px',
-        borderRadius: '5px',
-        fontWeight: '600',
-        backgroundColor: tab === value ? 'blue' : 'transparent',
-        color: tab === value ? 'white' : 'blue',
-        border: '1px solid blue',
-        cursor: 'pointer',
-        marginRight: '10px'
-      }}
-      onClick={() => setTab(value)}
-    >
-      {label}
-    </button>
-  );
+  let btn = document.getElementById("open-btn");
+
+  let button = document.getElementById("ok-btn");
+  function openModal() {
+    modal.style.display = "block";
+  }
+  window.onclick = function (event) {
+    if (event.target == modal && modal.style.display == "block") {
+      modal.style.display = "none";
+    }
+  };
 
   return (
-    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: '10%' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-        <Button label="User Details" value="userDetails" />
-        <Button label="Account Creation" value="accountCreation" />
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "10%",
+      }}
+    >
+      <div
+        class="fixed hidden inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20"
+        id="my-modal"
+      >
+        <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+          <div class="mt-3 text-center">
+            <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+              <svg
+                class="h-6 w-6 text-green-600"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 13l4 4L19 7"
+                ></path>
+              </svg>
+            </div>
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+              Valid user details
+            </h3>
+            <div class="mt-2 px-7 py-3">
+              <p class="text-sm text-gray-500">
+                Want to generate report?
+              </p>
+            </div>
+            <div class="flex  items-center px-4 py-3">
+              <button
+                id="ok-btn"
+                class="m-4 px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+              >
+                OK
+              </button>
+              <button
+                id="ok-btn"
+                class="m-4 px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div >
-        {tab === 'userDetails' && <UserDetails/>}
-        {tab === 'accountCreation' && <AccountCreation/>}
+      <div className="flex justify-center space-x-4 p-5">
+        <button
+          className={`px-4 py-2 rounded-lg shadow-md font-semibold ${
+            tab === "userDetails"
+              ? "bg-blue-500 text-white"
+              : "bg-white text-blue-500"
+          }`}
+          onClick={() => setTab("userDetails")}
+        >
+          User Details
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg shadow-md font-semibold ${
+            tab === "accountCreation"
+              ? "bg-blue-500 text-white"
+              : "bg-white text-blue-500"
+          }`}
+          onClick={() => setTab("accountCreation")}
+        >
+          Account Creation
+        </button>
+      </div>
+
+      
+      <div>
+        {tab === "userDetails" && <UserDetails openModal={openModal} />}
+        {tab === "accountCreation" && <AccountCreation />}
       </div>
     </div>
   );
--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 import "./UD.css";
-function UserDetails() {
+function UserDetails({openModal}) {
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
 
+  
+
   useEffect(() => {
-    try{
-    fetch("/userData.json?url")
-      .then((response) => response.json())
-      .then((data) => setUsers(data.users));
+    try {
+      fetch("/userData.json?url")
+        .then((response) => response.json())
+        .then((data) => setUsers(data.users));
+    } catch (error) {
+      console.log(error.message);
     }
-    catch(error){
-        console.log(error.message);
-    }
-    
   }, []);
 
   const filteredUsers = users.filter((user) =>
@@ -22,32 +22,83 @@ function UserDetails() {
   );
 
   return (
-    <div className="userdetails-outer">
-      <input
-        type="text"
-        placeholder="Search users"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <div className="table-responsive p-5 bg-gray-100 rounded-xl" >
-        <table className="w-full">
+    <div className="userdetails-outer ">
+      <div className="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-white  px-12">
+        <div className="flex justify-between">
+          <div className="inline-flex border rounded w-7/12 px-2 lg:px-6 h-12 bg-transparent">
+            <div className="flex flex-wrap items-stretch w-full h-full mb-6 relative">
+              <div className="flex">
+                <span className="flex items-center leading-normal bg-transparent rounded rounded-r-none border border-r-0 border-none lg:px-3 py-2 whitespace-no-wrap text-grey-dark text-sm">
+                  <svg
+                    width="18"
+                    height="18"
+                    className="w-4 lg:w-auto"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8.11086 15.2217C12.0381 15.2217 15.2217 12.0381 15.2217 8.11086C15.2217 4.18364 12.0381 1 8.11086 1C4.18364 1 1 4.18364 1 8.11086C1 12.0381 4.18364 15.2217 8.11086 15.2217Z"
+                      stroke="#455A64"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M16.9993 16.9993L13.1328 13.1328"
+                      stroke="#455A64"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </span>
+              </div>
+              <input
+                type="text"
+                className="flex-shrink flex-grow flex-auto leading-normal tracking-wide w-px flex-1 border border-none border-l-0 rounded rounded-l-none px-3 relative focus:outline-none text-xxs lg:text-xs lg:text-base text-gray-500 font-thin"
+                placeholder="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="table-responsive p-5 bg-gray-100 rounded-xl">
+        <table className="w-full table-auto">
           <thead className="bg-gray-50 border-b-2 border-gray-200 text-left ">
             <tr>
-              <th className="w-45 p-3 text-sm font-semibold tracking-wide text-left ">Username</th>
-              <th className="p-3 text-sm font-semibold tracking-wide text-left">Email</th>
-              <th className="w-25 p-3 text-sm font-semibold tracking-wide text-left">Phone</th>
-              <th className="w-12 p-3 text-sm font-semibold tracking-wide text-left">ID</th>
-              <th className="w-25 p-3 text-sm font-semibold tracking-wide text-left">Creation Date</th>
+              <th className="w-45 p-3 text-sm font-semibold tracking-wide text-left ">
+                Username
+              </th>
+              <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                Email
+              </th>
+              <th className="w-25 p-3 text-sm font-semibold tracking-wide text-left">
+                Phone
+              </th>
+              <th className="w-12 p-3 text-sm font-semibold tracking-wide text-left">
+                ID
+              </th>
+              <th className="w-25 p-3 text-sm font-semibold tracking-wide text-left">
+                Creation Date
+              </th>
             </tr>
           </thead>
           <tbody>
             {filteredUsers.map((user, i) => (
-              <tr key={user.id} className={i%2==0?"bg-white":"bg-gray-50"}>
+              <tr
+                key={user.id}
+                className={i % 2 == 0 ? "bg-white" : "bg-gray-50"}
+              onClick={openModal}>
                 <td className="p-3 text-sm text-gray-700">{user.username}</td>
-                <td className="email p-3 text-sm text-gray-700">{user.email}</td>
+                <td className="email p-3 text-sm text-gray-700">
+                  {user.email}
+                </td>
                 <td className="p-3 text-sm text-gray-700">{user.phone}</td>
                 <td className="p-3 text-sm text-gray-700">{user.id}</td>
-                <td className="p-3 text-sm text-gray-700">{user.creationDate}</td>
+                <td className="p-3 text-sm text-gray-700">
+                  {user.creationDate}
+                </td>
               </tr>
             ))}
           </tbody>
